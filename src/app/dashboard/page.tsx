a4b278d9a63cfe71f80bab05f06f177ef2019b6f
kaiser <kaiserzulkarnain@gmail.com>
2024-06-25 19:48:04 +0700

    feat: add logout feature

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-const dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    // If the session is not loaded yet, do nothing
+    // If the session is loaded and there's no user, redirect to login
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [session, status, router]);
+
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: "/login" });
+  };
+
+  return (
+    <div>
+      Dashboard
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
-export default dashboard;
+export default Dashboard;
